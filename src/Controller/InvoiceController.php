cd5507729ba37c5df9fe3ad5b854c66b47ecef36
Amadou Diallo <devamadoud@gmail.com>
2024-07-09 21:48:41 +0000

    Conflit correction

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class InvoiceController extends AbstractController
 {
     #[Route('/invoice', name: 'app_invoice')]
-    public function index(): Response
-    {
+    public function index(EntityManagerInterface $em): Response
+    {   
+        $user = $this->getUser();
+        
+        if (!($user instanceof User)) {
+            return $this->redirectToRoute('app_login');
+        }
+        
+    
         return $this->render('invoice/index.html.twig', [
             'controller_name' => 'InvoiceController',
         ]);
